8e9637a70bd2f036fb070e441ae16bd14e43ccae
Christian <christianpauld002@gmail.com>
2019-09-18 17:12:00 -0400

    Cleaned

--- ParkingPriceCalculator/src/prkingpricecalculator/PrkingPriceCalculator.java
+++ ParkingPriceCalculator/src/prkingpricecalculator/PrkingPriceCalculator.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 /**
  * To calculate the parking price
+ *
  * @author Christian
  */
 public class PrkingPriceCalculator {
+
     public static void main(String[] args) {
         // Getting input
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter parking time in minutes: ");
-        
+
         // Declaring variables
         double parkMin = input.nextInt();
         double parkHour = Math.ceil(parkMin / 60);
         double parkPrice = parkHour * 2;
-        
+
         // Printing
         System.out.println("");
         System.out.printf("%-15s: %.0f\n", "Parking in minutes", parkMin);
